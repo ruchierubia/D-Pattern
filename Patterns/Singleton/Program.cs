14c14476d165a0a1339e32abb8b7805f65a0bf2a
ruchier <ruchier@codev.com>
2019-10-16 01:53:14 +0800

    Rewrite singleton sample and singleton log example

---
+++ Patterns/Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // invoke parallel to check thread safe we create multiple instances
+            Parallel.Invoke(
+                    () => PrintA(),
+                    () => PrintB()
+            );
+        }
+
+        private static void PrintA()
+        {
+            //method 1
+            Singleton sg = Singleton.GetInstance;
+            sg.PrintDetails(sg.GetHashCode().ToString());
+        }
+
+        private static void PrintB()
+        {
+            // method 2
+            Singleton sg2 = Singleton.GetInstance;
+            sg2.PrintDetails(sg2.GetHashCode().ToString());
+
+        }
+    }
+}
---
+++ Patterns/Singleton/Singleton.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Singleton // sealed keyword is need to avoid nested derived class that can create multiple instances
+    {
+        public static readonly Lazy<Singleton> instance = new Lazy<Singleton>(
+          () => new Singleton()
+        );// To make it lazy loading Lazy object are thread safe
+          // private static readonly object obj = new object(); // use as lock object to wrap instance creatio
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance
+        {
+
+            //if (instance == null)
+            //    instance = new Singleton();
+
+            //return instance;
+            get
+            {
+                //if (instance == null) //double checked locking
+                //{
+                //    lock (obj) // lock to make sure only on thread can enter the code block at any given time
+                //    {
+                //        if (instance == null)
+                //            instance = new Singleton();
+                //    }
+                //}
+
+                return instance.Value;
+
+            }
+        }
+
+        public void PrintDetails(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+
+
+        /*
+         * Lazy loading vs eager loading
+         * 
+         * 
+         * 
+         * Static vs singleton
+         * static only provide methods singleton is object
+         * 
+         * singleton uses realworld
+         * 
+         * logging
+         * manage connection to database
+         * printer pooling
+         * file management
+         * configuration
+         * cache
+         * session base shopping cart
+         */
+    }
+}
